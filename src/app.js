af16104916cd2fb82b68e1c21fdf6c18590751d0
John Davis <johnmdavis125@gmail.com>
2022-09-29 09:14:48 -0400

    configured .env support for custom port number in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
+const PORT = process.env.PORT || 3001; 
 const app = express(); 
-
 
 const cors = require('cors'); 
 app.use(cors({
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api', require('./Controllers/messages_controller')); 
 
 app.get('/', (req, res)=>{
-    res.status(200).send('express get request to base URL @ http://localhost:3001. Server standing by to fulfill requests from authorized client(s)');
+    res.status(200).send(`express get request to base URL @ http://localhost:${PORT}. Server standing by to fulfill requests from authorized client(s)`);
 });
 
 module.exports = app; 
